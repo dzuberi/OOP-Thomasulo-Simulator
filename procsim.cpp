140fa530722f0aec7433b772711a3b0191321ee2
Danial <dzuberi522@gmail.com>
2018-03-25 23:50:13 -0500

    state update complete i think

--- procsim.cpp
+++ procsim.cpp
@@ -17,14 +17,17 @@ typedef struct _preg{
 } pregEntry;
 
 typedef struct _robEntry{
+	int occupied;
 	int32_t busy;
 	int32_t areg;
 	int32_t prevPreg;
 	int32_t preg;
+	uint32_t instruction_address;
 } robEntry;
 
 typedef struct _schedEntry{
 	int32_t busy;
+	uint32_t instruction_address;
 	int32_t dest_areg;
 	int32_t dest_preg;
 	int32_t src1_preg;
@@ -36,42 +39,68 @@ public:
 	state_update() {}
 	void init(uint64_t size){
 		robSize = size;
+		robEnd = (int) size - 1;
 		rob_ = (robEntry*) calloc(0,sizeof(robEntry) * size);
 		printf("rob created\n");
+		numFree = (int) size;
 	}
 
-	void update(){
-		for(int i=robSize; i>0; i--){
-			if(rob_[i].busy == 0){
-
+	void update(pregEntry* regFile){
+		int freeable = -1;
+		while(rob_[robEnd].busy == 0){
+			freeable = this -> shiftForward();
+			if(freeable != -1){
+				regFile[freeable].busy = 0;
 			}
-			else break;
+			if(rob_[robEnd].occupied == 1){
+				numFree += 1;
+			}
 		}
 	}
 
-	int addEntry(robEntry* newEntry){
+	int shiftForward(){
 		int preg_to_free = -1;
-		for(int i=robSize; i>0; i--){
-			if(i == robSize && (rob_[i].prevPreg != rob_[i].preg)){
-				preg_to_free = rob_[i].prevPreg;
-			}
+		if((rob_[robEnd].prevPreg != rob_[robEnd].preg) && (rob_[robEnd].busy == 0) && (rob_[robEnd].occupied == 1)){
+			preg_to_free = rob_[robEnd].prevPreg;
+		}
+		for(int i=robEnd; i>0; i--){
 			rob_[i] = rob_[i-1];
 		}
-		rob_[0] = *newEntry;
+		rob_[0].busy = 0;
+		rob_[0].occupied = 0;
 		return preg_to_free;
 	}
+
+	void addEntry(robEntry * newEntry){
+		int highestFree = -1;
+		for(int i=0; i<((int)robSize); i++){
+			if(rob_[i].occupied == 0){
+				if(i > highestFree){
+					highestFree = i;
+				}
+			}
+		}
+		rob_[highestFree] = *newEntry;
+		rob_[highestFree] = {.occupied = 1, .busy = 1};
+		numFree -= 1;
+	}
+	
 	robEntry* rob_;
+	uint64_t robSize;
+	int robEnd;
+	int numFree;
 private:
-	int robSize;
 };
 
 class scheduler_ {
 public:
 	scheduler_() {}
 	void init(uint64_t size){
+		schedSize = size;
 		schedQ = (schedEntry*) calloc(0,sizeof(schedEntry) * size);
 		printf("scheduling queue created\n");
 	}
+	uint64_t schedSize;
 	schedEntry* schedQ;
 private:
 };
@@ -80,9 +109,11 @@ class physFile {
 public:
 	physFile() {}
 	void init(uint64_t size){
+		pregSize = size;
 		pregFile = (pregEntry*) calloc(0,sizeof(pregEntry) * size);
 		printf("preg file created\n");
 	}
+	uint64_t pregSize;
 	pregEntry* pregFile;
 private:
 };
@@ -126,7 +157,7 @@ void run_proc(proc_stats_t* p_stats){
 	while(read_instruction(&instruction)){
 		rob.addEntry(&newentry);
 		int32_t opcode = instruction.op_code;
-		if(opcode = -1){
+		if(opcode == -1){
 			opcode = 1;
 		}
 		uint32_t address = instruction.instruction_address;
@@ -134,6 +165,7 @@ void run_proc(proc_stats_t* p_stats){
 		int32_t src2 = instruction.src_reg[1];
 		int32_t dest = instruction.dest_reg;
 		//printf("%d\n", opcode);
+		rob.update(pregs.pregFile);
 
 	}
 }
